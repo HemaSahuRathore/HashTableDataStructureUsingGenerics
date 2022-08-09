06f01847a22c455e181d3f306882b6de89829b09
Hema <hemasahu019@gmail.com>
2022-08-09 23:24:37 +0530

    Added Welcome page code to main

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HashTableNBinaryTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Hash table and BST Data Structures using Generics");
+        }
+    }
+}
